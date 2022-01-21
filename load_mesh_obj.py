d1e83a9c26e6ebe8bff6a3db47e5d899bf54aa7c
Yong <qbxlvnf11@naver.com>
2022-01-21 14:38:37 +0900

    Update load_mesh_obj.py

--- load_mesh_obj.py
+++ load_mesh_obj.py
@@ -1,16 +1,19 @@
 from pytorch3d.io import load_obj
+import argparse
 
 def parse_args():
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--video_path', help='input mesh data (obj) path',
+	parser.add_argument('--obj_path', help='input mesh data (obj) path',
 		default='./processed/zilla_night_black_heat/textured_meshes/optimized_poisson_texture_mapped_mesh.obj') # Dataset: BigBIRD
+	
+	args = parser.parse_args()
 
 	return args
 
 args = parse_args()
 
 # Load mesh data with object format
-verts, faces, properties = load_obj(args.path)
+verts, faces, properties = load_obj(args.obj_path)
 print('verts:', verts) # v
 print('faces:', faces) # f
 print('normals:', properties.normals) # vn
